3e54d496b1f3129c3860837a27d60f169d247880
MRyutaro <m2002r1028@icloud.com>
2023-10-19 15:57:27 +0900

    fix: インデント

--- sql/1.sql
+++ sql/1.sql
@@ -1,6 +1,6 @@
-select (
-    select name from subjects where id = subject_id
-) as subject_name, avg(score) as avg_score
+select 
+    (select name from subjects where id = subject_id) as subject_name,
+    avg(score) as avg_score
 from scores
 group by subject_id
 order by avg_score desc;
--- sql/2.sql
+++ sql/2.sql
@@ -1,6 +1,6 @@
-select (
-    select name from students where id = student_id
-), avg(score) as avg_score
+select 
+    (select name from students where id = student_id),
+    avg(score) as avg_score
 from scores
 group by student_id
 order by avg_score desc;
