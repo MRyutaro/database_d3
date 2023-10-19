e3289a8aefd504c70c1a3a2529638bc446cfc905
MRyutaro <m2002r1028@icloud.com>
2023-10-19 15:34:55 +0900

    del: テーブル一覧

--- sql/2.sql
+++ sql/2.sql
@@ -1,10 +1,3 @@
-\dt
-
-select * from students;
-select * from subjects;
-select * from scores;
-select * from scores_grades;
-
 select (
     select name from students where id = student_id
 ), avg(score) as avg_score
