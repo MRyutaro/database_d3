c747b6d9e7f3a88cbf4d90abdb89bfd9009a0b96
MRyutaro <m2002r1028@icloud.com>
2023-10-19 15:33:29 +0900

    fix: 順番

--- sql/1.sql
+++ sql/1.sql
@@ -2,4 +2,5 @@ select (
     select name from subjects where id = subject_id
 ) as subject_name, avg(score) as avg_score
 from scores
-group by subject_id;
+group by subject_id
+order by avg_score desc;
