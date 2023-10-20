3a3cd246633ffa7a6a02b5f7e6c88915b8b790ee
MRyutaro <m2002r1028@icloud.com>
2023-10-20 21:41:12 +0900

    fix: 氏名、科目名、スコア、成績

--- sql/3.sql
+++ sql/3.sql
@@ -1,5 +1,6 @@
 select
     student_name,
+    subject_name,
     score,
     grade
 from (
@@ -9,11 +10,16 @@ from (
             from students
             where id = student_id
         ) as student_name,
-        score,
         (
             select grade
             from scores_grades
             where score >= min_score and score <= max_score
-        ) as grade
+        ) as grade,
+        (
+            select name
+            from subjects
+            where id = subject_id
+        ) as subject_name,
+        score
     from scores
 ) as subquery
