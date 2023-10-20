38fc137970b437f010d3f3e8c19db38db46cae39
MRyutaro <m2002r1028@icloud.com>
2023-10-20 21:37:35 +0900

    fix: スコアと成績を取得できるようにした

--- sql/3.sql
+++ sql/3.sql
@@ -1,22 +1,19 @@
-\dt
-
-select * from students;
-select * from subjects;
-select * from scores;
-select * from scores_grades;
-
 select
     student_name,
-    sum(credit_unit) as total_credit_units
+    score,
+    grade
 from (
-    -- TODO: if文を使ってgradeがFだった場合にはcredit_unit=0, それ以外の場合にはcredit_unitにする
     select
-        (select name from students where id = student_id) as student_name,
         (
-            select credit_unit
-            from subjects
-            where id = subject_id
-        ) as credit_unit
+            select name
+            from students
+            where id = student_id
+        ) as student_name,
+        score,
+        (
+            select grade
+            from scores_grades
+            where score >= min_score and score <= max_score
+        ) as grade
     from scores
 ) as subquery
-group by student_name;
